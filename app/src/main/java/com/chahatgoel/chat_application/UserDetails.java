96aeb407ff2ec4b0ec1072784cac963d9dd4d0c0
Chahat Goel <chahatgoel98@gmail.com>
2018-01-30 22:25:13 +0530

    commit

---
+++ app/src/main/java/com/chahatgoel/chat_application/UserDetails.java
@@ -0,0 +1,11 @@
+package com.chahatgoel.chat_application;
+
+/**
+ * Created by Chahat goel on 30-01-2018.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
