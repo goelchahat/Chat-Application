42755c672bd2b48d7d0f6254d43a576b29d79c5a
Chahat Goel <chahatgoel98@gmail.com>
2018-01-30 22:11:31 +0530

    commit

---
+++ app/src/main/java/com/chahatgoel/chat_application/Login.java
@@ -0,0 +1,13 @@
+package com.chahatgoel.chat_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
